8bc4a6b6bde6efa15662c90e21ca6c063ffa32ea
Jay Chen <chenjainjay@gmail.com>
2022-08-21 02:48:47 +0800

    environment setting file

---
+++ my-profile.ps1
@@ -0,0 +1,8 @@
+# Prompt
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\craver.omp.json" | Invoke-Expression
+Import-Module -Name Terminal-Icons
+# Alias
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias dd dotnet
+Set-Alias op 'start .'
---
+++ my-terminal-setting.ps1
@@ -0,0 +1,3 @@
+Install-Module posh-git -Scope CurrentUser -Force
+Install-Module oh-my-posh -Scope CurrentUser -Force
+Install-Module -Name Terminal-Icons -Repository PSGallery
