542c627e2764f865c26799981d8fcb2e2a90831d
Jay Chen <chenjainjay@gmail.com>
2022-08-26 00:06:10 +0800

    Update my-profile.ps1

--- my-profile.ps1
+++ my-profile.ps1
@@ -1,5 +1,6 @@
 # Prompt
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\craver.omp.json" | Invoke-Expression
+Import-Module posh-git
 Import-Module -Name Terminal-Icons
 # Alias
 Set-Alias ll ls
