550a3365a2880c5ebe76632f003eb527593e5387
Jay Chen <chenjainjay@gmail.com>
2022-08-28 02:06:43 +0800

    Update my-profile.ps1

--- my-profile.ps1
+++ my-profile.ps1
@@ -1,11 +1,13 @@
 # Prompt
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\craver.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\craver.omp.json" | Invoke-Expression
 #Import-Module posh-git
 Import-Module -Name Terminal-Icons
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin 
 
 #StarShip
-# $ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
-# Invoke-Expression (&starship init powershell)
+$ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+Invoke-Expression (&starship init powershell)
 # Alias
 Set-Alias ll ls
 Set-Alias g git
