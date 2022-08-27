586ee3eabc38a70682b9462fbcaacdd1d8529c38
Jay Chen <chenjainjay@gmail.com>
2022-08-28 02:08:48 +0800

    Update my-terminal-setting.ps1

--- my-terminal-setting.ps1
+++ my-terminal-setting.ps1
@@ -1,3 +1,5 @@
 Install-Module posh-git -Scope CurrentUser -Force
 Install-Module oh-my-posh -Scope CurrentUser -Force
 Install-Module -Name Terminal-Icons -Repository PSGallery
+Install-Module PSReadLine -AllowPrerelease -Force
+winget install --id Starship.Starship
