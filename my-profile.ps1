6b0a4736fdb43d0950494d2c4a671b64d042e2f3
Jay Chen <chenjainjay@gmail.com>
2022-08-27 17:44:25 +0800

    Update my-profile.ps1

--- my-profile.ps1
+++ my-profile.ps1
@@ -1,9 +1,15 @@
 # Prompt
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\craver.omp.json" | Invoke-Expression
-Import-Module posh-git
+#Import-Module posh-git
 Import-Module -Name Terminal-Icons
+
+#StarShip
+# $ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+# Invoke-Expression (&starship init powershell)
 # Alias
 Set-Alias ll ls
 Set-Alias g git
 Set-Alias dd dotnet
 Set-Alias op start
+# Default Location
+#Set-Location E:\Git
